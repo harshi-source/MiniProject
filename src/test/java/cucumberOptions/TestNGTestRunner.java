733be9ad7651ca2f02b8f6832b1763e348ee3be7
Harshitha <harshithasuraparaj228@gmail.com>
2025-01-03 20:49:08 +0530

    Extent spark html report

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -3,9 +3,9 @@ package cucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
-
-@CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",monochrome = true,tags="@placeOrder or @searchItem",
-plugin={"html:target/report.html","json:target/report.json"})
+//we add cucumber adaptor plugin for generating extent reports, taken reference from https://extentreports.com/docs/versions/4/java/cucumber4.html
+@CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",monochrome = true,tags="@placeOrder",
+plugin={"html:target/report.html","json:target/report.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 //when we run parallel,using TestNG, it will open multiple browsers at a time and executes all the test simultaneously
     @Override
