2aec22966c5b97a6072b37c1c5169f2afa28dcb3
Harshitha <harshithasuraparaj228@gmail.com>
2025-01-03 15:23:10 +0530

    parallel execution using TestNG DataProvider annotation

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -2,8 +2,15 @@ package cucumberOptions;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",monochrome = true)
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+    //parallel execution using TestNG triggers each scenarios in different tabs but it triggers scenarios in different windows when it runs in sequentially
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
 
 }
