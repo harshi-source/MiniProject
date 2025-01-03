f4b0126f462e19751f03b01c994ca9436c201775
Harshitha <harshithasuraparaj228@gmail.com>
2025-01-03 19:58:45 +0530

    inbuilt reporting techniques provided by cucumber framework

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -4,9 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",monochrome = true)
+@CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",monochrome = true,tags="@placeOrder or @searchItem",
+plugin={"html:target/report.html","json:target/report.json"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
+//when we run parallel,using TestNG, it will open multiple browsers at a time and executes all the test simultaneously
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios(){
