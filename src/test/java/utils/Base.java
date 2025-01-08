84e4bf4e48887c969c95080f61c31c8fb9464fd3
Harshitha <harshithasuraparaj228@gmail.com>
2025-01-08 19:24:32 +0530

    running maven commands through terminal or cmd prompt

--- src/test/java/utils/Base.java
+++ src/test/java/utils/Base.java
@@ -19,23 +19,30 @@ public class Base {
         Properties prop =new Properties();
         prop.load(fis);
         String url= prop.getProperty("QAUrl");
+        //this line will get the browser value from the global properties file in our framework
+        String browser_properties=prop.getProperty("browser");
+        //This line will get the value passed from the terminal or command prompt related to browser to run maven commands from the terminal or cmd
+        String browser_maven=System.getProperty("browser");
+        //result=test condition ? value1: value2;
+        String browser= browser_maven!=null?browser_maven:browser_properties;
+
         if(driver==null) {
 
             //driver = new ChromeDriver();
             //driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-            if(prop.getProperty("browser").equalsIgnoreCase("chrome")){
+            if(browser.equalsIgnoreCase("chrome")){
+
                 driver= new ChromeDriver();
                 //driver.manage().window().maximize();
 
 
             }
-//            else if(prop.getProperty("browser").equalsIgnoreCase("firefox")){
-//                //driver= new FirefoxDriver();
+            if(browser.equalsIgnoreCase("firefox")){
+                driver= new FirefoxDriver();
 //                //driver.manage().window().maximize();
-//
-//
-//            }
-            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+           }
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.get(url);
 
         }
@@ -43,3 +50,4 @@ public class Base {
         return driver;
     }
 }
+//follow this site for more insights about cucumber- https://cucumber.io/docs/cucumber/api
